f6b1f6caefbea5e3c35a2c1e36d0e0758981b590
kiemluc01 <kiemluc01@gmail.com>
2023-01-10 16:31:30 +0700

    link profile

--- resources/views/static-layout/header.blade.php
+++ resources/views/static-layout/header.blade.php
@@ -117,7 +117,7 @@
                         aria-labelledby="navbarDropdownMenuAvatar"
                     >
                         <li>
-                            <a class="dropdown-item" href="#"><i class="fa-solid fa-user"></i>Tài khoản của tôi </a>
+                            <a class="dropdown-item" href="/profile"><i class="fa-solid fa-user"></i>Tài khoản của tôi </a>
                         </li>
                         <li>
                             <a class="dropdown-item" href="/logout"><i class="fa-solid fa-right-from-bracket"></i>Đăng xuất</a>
