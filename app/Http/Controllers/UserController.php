d50e18312cc4a178ba11058c67ba4be99d677d22
Hoang-149 <thanghoangminh149@gmail.com>
2023-01-10 14:24:50 +0700

    haha

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -16,7 +17,13 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $cookie = new CookieController();
+        $id = $cookie->get('user');
+
+        $user = DB::table('users')->find($id);
+
+        // dd($user);
+        return view('non-static-layout.profile', compact('user'));
     }
 
     /**
@@ -71,7 +78,21 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $cookie = new CookieController();
+        $id = $cookie->get('user');
+
+        $user = DB::table('users')->find($id);
+
+        $user->first_name = $request->hoten;
+
+        $user->email = $request->email;
+
+        $user->sdt = $request->sdt;
+        $user->password = $request->password;
+
+        $user->save();
+
+        // return redirect()->route('profile.index')->with('success', 'User Data has been updated successfully');
     }
 
     /**
@@ -85,75 +106,72 @@ class UserController extends Controller
         //
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $link = "/";
         $Cookie = new CookieController();
-        if($Cookie->check('url'))
+        if ($Cookie->check('url'))
             $link = $Cookie->get('url'); //return link before login
         $username = $request->input('username');
         $password = $request->input('password');
-        if(User::query()->where('username', $username)->where('password', $password)->exists())
-        {
+        if (User::query()->where('username', $username)->where('password', $password)->exists()) {
             $user = User::query()->where('username', $username)->where('password', $password)->first();
-            if($Cookie->setUser($user->id))
-            {
+            if ($Cookie->setUser($user->id)) {
                 $Cookie->set('last_user', $username);
                 $Cookie->delete('password');
-                if($request->has('remember'))
-                {
+                if ($request->has('remember')) {
                     $Cookie->set('password', $password);
                 }
-                return redirect($link)->with('success','đăng nhập thành công');
+                return redirect($link)->with('success', 'đăng nhập thành công');
             }
         }
-        return redirect('/login')->with('login fail','sai tài khoản hoặc mật khẩu');
+        return redirect('/login')->with('login fail', 'sai tài khoản hoặc mật khẩu');
     }
 
     public function register(Request $request)
     {
-        if(User::query()->where('username',$request->input('username'))->exists())//check user 
-            return redirect('register')->with('fail','username đã tồn tại');
+        if (User::query()->where('username', $request->input('username'))->exists()) //check user 
+            return redirect('register')->with('fail', 'username đã tồn tại');
         $username = $request->input('username');
         $first_name = $request->input('first_name');
         $cf_password = $request->input('cf_password');
         $password = $request->input('password');
-        if($password == $cf_password)
-        {
+        if ($password == $cf_password) {
             $user = new User();
             $user->first_name = $first_name;
             $user->username = $username;
             $user->password = $password;
-            $user->save();
-            if($request->has('remember'))
-                return redirect('/login')->with('message','đăng kí thành công')->with('username',$username)->with('password', $password);
-            return redirect('/login')->with('message','đăng kí thành công');
-        }
-        return redirect('register')->with('fail','mật khẩu không giống nhau');
 
+            $user->save();
+            if ($request->has('remember'))
+                return redirect('/login')->with('message', 'đăng kí thành công')->with('username', $username)->with('password', $password);
+            return redirect('/login')->with('message', 'đăng kí thành công');
+        }
+        return redirect('register')->with('fail', 'mật khẩu không giống nhau');
     }
 
     public function logout()
     {
         $Cookie = new CookieController();
-        $Cookie->deleteUser();//logout => delete cookie 
+        $Cookie->deleteUser(); //logout => delete cookie 
         return redirect('/login');
     }
 
     public static function getName($id)
     {
-        $user = User::query()->where('id',$id)->first();
-        if($user->first_name !== null || $user->last_name !== null)
-            return $user->last_name.' '.$user->first_name;
+        $user = User::query()->where('id', $id)->first();
+        if ($user->first_name !== null || $user->last_name !== null)
+            return $user->last_name . ' ' . $user->first_name;
         return $user->username;
     }
 
     public static function checkLogin()
     {
         $cookie = new CookieController();
-        if($cookie->check('user'))
-            if($cookie->check('url'))
-                return redirect($cookie->get('url'))->with('message'.'bạn đã đăng nhập');
-            return redirect('/')->with('message'.'bạn đã đăng nhập');
+        if ($cookie->check('user'))
+            if ($cookie->check('url'))
+                return redirect($cookie->get('url'))->with('message' . 'bạn đã đăng nhập');
+        return redirect('/')->with('message' . 'bạn đã đăng nhập');
     }
 
     public static function rootImage($id)
--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,9 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('non-static-layout.register');
 });
+
 //information
-Route::get('/profile', function () {
-    return view('non-static-layout.profile');
-});
+Route::resource('profile', UserController::class);
 
 
 Route::get('/send-mail', function () {
@@ -45,33 +44,33 @@ Route::get('/send-mail', function () {
         'title' => 'Thông Báo Về Tin Mới Trong TIN TỨC EXPRESS',
         'body' => 'Đã có thông báo mới trong bảng tin, xin mời bạn vào xem!!!'
     ];
-    $mail_to = ["kiemluc01@gmail.com","nvthuan1201@gmail.com","nguyenquocvu10a2@gmail.com","Thanghoangminh149@gmail.com","buivietthinh.qng97@gmail.com","thanhhung14072001@gmail.com"];
-    foreach($mail_to as $mail){
+    $mail_to = ["kiemluc01@gmail.com", "nvthuan1201@gmail.com", "nguyenquocvu10a2@gmail.com", "Thanghoangminh149@gmail.com", "buivietthinh.qng97@gmail.com", "thanhhung14072001@gmail.com"];
+    foreach ($mail_to as $mail) {
         Mail::to(strval($mail))->send(new Sendmail($details));
     }
-     
+
     dd("Email is Sent.");
 });
 
 //User controller
-Route::controller(UserController::class)->group(function(){
-    Route::post('/login', 'login');//login method
-    Route::post('/register', 'register');//register method
+Route::controller(UserController::class)->group(function () {
+    Route::post('/login', 'login'); //login method
+    Route::post('/register', 'register'); //register method
     Route::get('logout', 'logout'); //method logout
 });
 
-Route::controller(SocialController::class)->group(function(){
-    Route::get('/getInfor/{social}', 'getInfo');//login method
-    Route::get('/checkInfor/{social}', 'checkInfo');//register method
+Route::controller(SocialController::class)->group(function () {
+    Route::get('/getInfor/{social}', 'getInfo'); //login method
+    Route::get('/checkInfor/{social}', 'checkInfo'); //register method
 });
 
-Route::controller(PostController::class)->group(function(){
-    Route::get('/','index');
-    Route::get('/details/{id}','show');
+Route::controller(PostController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/details/{id}', 'show');
     Route::post('/search', 'search');
     Route::get('/search/category/{id_cat}', 'search_cat');
 });
- 
+
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
@@ -84,13 +83,12 @@ Route::get('/admin/post', function () {
     return view('admin.post');
 });
 
-Route::controller(CommentController::class)->group(function(){
+Route::controller(CommentController::class)->group(function () {
     Route::post('details/{id}/comment', 'store');
     Route::get('details/{id}/deleteComment/{idcmt}', 'destroy');
 });
 
-Route::controller(LikeController::class)->group(function(){
+Route::controller(LikeController::class)->group(function () {
     Route::get('details/{id}/like/{idcmt}', 'store');
     Route::get('details/{id}/unlike/{idcmt}', 'destroy');
 });
-
